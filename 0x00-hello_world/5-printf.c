7414a44498e8ebae7bc937de6e6799697a9e5bf6
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-06-15 19:47:46 +0000

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,9 @@
  * Description: Print a sentence
  *
  * Return: null
- * 
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n";
-	return (0);)
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
