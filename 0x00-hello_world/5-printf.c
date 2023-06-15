a3d253998ffbdeb85b1d0297afb03170ab7059b2
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-06-15 19:45:05 +0000

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Print a sentence
+ *
+ * Return: null
+ * 
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n";
+	return (0);)
+}
