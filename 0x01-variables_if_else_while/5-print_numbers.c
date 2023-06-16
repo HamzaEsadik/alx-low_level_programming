10221c8697c0b1f3a5afe780e1172e3a47ce5234
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-06-16 14:37:26 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print digit numbers of base 10
+ *
+ * Return: null
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
