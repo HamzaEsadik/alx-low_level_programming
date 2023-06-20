12c38f71e5b814d8af8f032291a94ab62befca26
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-06-20 17:31:13 +0000

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - jack_bauer time
+ *
+ * Return: null
+ */
+void jack_bauer(void)
+{
+	for (int h = 0; h < 24; h++)
+	{
+		for (int m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + 48);
+			_putchar((h % 10) + 48);
+			_putchar(':');
+			_putchar((m / 10) + 48);
+			_putchar((m % 10) + 48);
+			_putchar('\n');
+		}
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,5 +10,6 @@ int print_last_digit(int);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
+void jack_bauer(void);
 
 #endif
