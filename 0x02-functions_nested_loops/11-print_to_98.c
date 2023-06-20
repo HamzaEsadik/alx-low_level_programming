3e7ff8b06b3f7120df70c33f8dee422147feecde
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-06-20 18:40:53 +0000

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#inclide "main.h"
+
+/**
+ * print_to_98 - print numbers to 98
+ * @n: start count from n
+ *
+ * Return: null
+ */
+void print_to_98(int n)
+{
+	for (n; n <= 98; n++)
+	{
+		printf("%d, ", n);
+	}
+	for (n; n >= 98; n--)
+	{
+		printf("%d, ", n);
+	}
+}
