ef108d84dbcc1863c7a11e8e789919a282891acf
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-06-20 16:38:53 +0000

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ *
+ * @i: last digit
+ *
+ * Return: null
+ */
+int print_last_digit(int i)
+{
+	int n = i % 10;
+
+	return (n);
+}
