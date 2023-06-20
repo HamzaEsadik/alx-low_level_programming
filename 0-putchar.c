fd258b4e3b325902820028e1e3ae859b2aeb49fb
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-06-20 12:23:56 +0000

    task 0

---
+++ 0-putchar.c
@@ -0,0 +1,21 @@
+#include "_putchar.c"
+
+/*
+ * * main - main fuction
+ * *
+ * * Description: print  _putchar
+ * *
+ * * Return: null
+ * */
+int main() {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
