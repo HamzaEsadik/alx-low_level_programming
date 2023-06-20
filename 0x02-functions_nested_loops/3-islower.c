9c1c2e1061ea9e8b3b9f31ec96f1c18ce1d80d6a
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-06-20 14:50:53 +0000

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - check if a character is lower
+ *
+ * Return: null
+ */
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
