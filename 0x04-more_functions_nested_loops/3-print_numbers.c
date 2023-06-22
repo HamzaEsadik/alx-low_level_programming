826df9dc1188511c4854a271b8237034d6205f00
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-06-22 13:25:03 +0000

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ * Return: null
+ */
+int print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+	return (0);
+}
