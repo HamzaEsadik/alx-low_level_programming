0eedef58286b7bdff6bfea8cef8942b922ea1beb
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-06-22 12:24:50 +0000

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _isupper - check if a character is uppercase
+ * @c: character
+ * Return: 1 if upper 0 if lower
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		Return(1);
+	} else
+	{
+		Return(0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+int _isupper(int c);
+
+
+
+#endif
