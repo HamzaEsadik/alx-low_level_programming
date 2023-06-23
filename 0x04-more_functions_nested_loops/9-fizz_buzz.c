5a931b316f7595f66b314c18e20f60c94b572920
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-06-23 00:03:34 +0000

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,26 +10,21 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && !(i % 5 == 0))
+		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz");
-		} else if (i % 5 == 0 && !(i % 3 == 0))
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		} else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
+		} else if (i == 1)
+		{
+			printf("%d", i);
 		} else
 		{
 			printf("%d ", i);
-		}
-		if (i != 100)
-		{
-			printf(" ");
-		}
-		else
-		{
-			printf("\n");
 		}
 	}
 	printf("\n");
