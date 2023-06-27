f5891eaedddaf51cf9a70e1be92fd754fd7766d6
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-06-27 00:49:14 +0000

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,13 +11,12 @@ void rev_string(char *s)
 	int i;
 	char temp;
 
-	for (l = 0; s[1] != '\0'; ++l)
-		;
-
-	for (i = 0; i < l / 2; i++)
+	while (s[i++])
+		l++;
+	for (i = l - 1; i >= l / 2; i--)
 	{
 		temp = s[i];
-		s[i] = s[l - 1 - i];
-		s[l - 1 - i] = temp;
+		s[i] = s[l - i - 1];
+		s[len - i - 1] = temp;
 	}
 }
