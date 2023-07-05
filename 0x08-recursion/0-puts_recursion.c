f78bda5ff8b2826ec75a2128d5544efab6d94e16
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-07-05 12:43:53 +0000

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - puts a string using recursion
+ * @s: arg
+ * Return: null
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_putchar(*s + 0);
+	_puts_recursion(s + 1);
+	_putchar('\n');
+}
