e1393ce9b27b73cdaeeec4a9f79e15675923686e
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-07-10 18:04:37 +0000

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strdup - allocate new space in memory
+ * and assign a string to it.
+ * @str: a string
+ * Return: Pointer to the string
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int lenghOfStr;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	lenghOfStr = strlen(str);
+	p = (char *)malloc(sizeof(char) * lenghOfStr);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < lenghOfStr; i++)
+	{
+		*(p + i) = str[i];
+	}
+	return (p);
+}
