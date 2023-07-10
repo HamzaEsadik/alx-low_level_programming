b21d9c7a2392a372a77586dd189015847fe7ba0d
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-07-10 21:03:01 +0000

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,11 +18,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		*s1 = "";
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		*s2 = "";
+		s2 = "";
 	}
 
 	p = (char *)malloc(lenghtOfStr + 1);
