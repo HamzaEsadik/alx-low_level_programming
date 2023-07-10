52fbb17b7c87f7b6c14a7c5c9e53607ea6450f1f
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-07-10 20:56:18 +0000

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - Merge 2 strings
+ * @s1: first string
+ * @s2: second string
+ * Return: Merged string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	int lenghOfS1, lenghOfS2, lenghtOfStr;
+
+	lenghOfS1 = strlen(s1);
+	lenghOfS2 = strlen(s2);
+	lenghtOfStr = lenghOfS1 + lenghOfS2;
+
+	p = (char *)malloc(lenghtOfStr + 1);
+
+	strcpy(p, s1);
+	strcpy(p + lenghOfS1, s2);
+
+	return (p);
+}
