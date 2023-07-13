b478c8ba269686380383e81b3dd5295f747b1a50
Gnomedebian <yassineamgarou@gmail.com>
2023-07-13 17:44:13 +0000

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - calloc func using malloc
+ * @nmemb: Array length
+ * @size: size of each element
+ * Return: pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb; i++)
+	{
+		*(p + i) = 0;
+	}
+
+	return (p);
+}
