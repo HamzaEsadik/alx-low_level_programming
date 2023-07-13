1cdeb6a58b497c16070cc3ee3ce3c1e7cd57df1a
Gnomedebian <yassineamgarou@gmail.com>
2023-07-13 16:56:01 +0000

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,27 +11,28 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i;
+	unsigned int i;
 	char *p;
-	int lens1 = strlen(s1);
-	int lens2 = strlen(s2);
+	unsigned int lens1 = strlen(s1);
+	unsigned int lens2 = strlen(s2);
+	int l = lens1 + n;
 
 	if (s1 == NULL)
 	{
 		s1 = "";
 	}
+
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
 
-	if (n >= lens2)
+	if (n > lens2)
 	{
-		p = (char *)malloc(lens1 + lens2 + 1);
-	} else
-	{
-		p = (char *)malloc(lens1 + n + 1);
+		n = lens2;
 	}
+
+	p = (char *)malloc(lens1 + n + 1);
 
 	for (i = 0; i < lens1; i++)
 	{
@@ -42,6 +43,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		*(p + lens1 + i) = s2[i];
 	}
+
+	*(p + l) = '\0';
+
 	if (p == NULL)
 	{
 		return (NULL);
