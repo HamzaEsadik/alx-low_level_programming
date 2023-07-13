3457ecb3ccbca5fbac9fee30006293c66cf66cfe
Gnomedebian <yassineamgarou@gmail.com>
2023-07-13 15:36:00 +0000

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory
+ * @b: space allocated
+ * Return: pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *p = (void *)malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+Readme file for 0x0C-more_malloc_free
