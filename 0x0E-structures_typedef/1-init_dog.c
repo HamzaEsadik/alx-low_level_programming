acd4afd59b7815272d521a45357fe491f36e9627
Gnomedebian <yassineamgarou@gmail.com>
2023-07-17 20:01:14 +0000

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a structure
+ * @d: struct
+ * @name: his name
+ * @age: his age
+ * @owner: his owner
+ * Return: null
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
