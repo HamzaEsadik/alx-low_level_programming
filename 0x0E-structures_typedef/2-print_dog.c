4ec322528f01f6145f17efb0dee25838ef8442d9
Gnomedebian <yassineamgarou@gmail.com>
2023-07-17 20:48:15 +0000

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,18 +11,17 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if (d->name != NULL)
-			printf("Name: %s\n",d->name);
+			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
 		if (d->age > 0)
-			printf("Age: %f\n",d->age);
+			printf("Age: %f\n", d->age);
 		else
 			printf("Age: (nil)\n");
 
 		if (d->owner != NULL)
-			printf("Owner: %s\n",d->owner);
+			printf("Owner: %s\n", d->owner);
 		else
 			printf("Owner: (nil)\n");
-		            
 	}
 }
