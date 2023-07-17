5cfb5d793bf14f0b46cae7fa5b82ada008c4b86c
Gnomedebian <yassineamgarou@gmail.com>
2023-07-17 20:47:25 +0000

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
-#include <stdlib.ih>
+#include <stdlib.h>
 
 /**
  * print_dog - print strct values
@@ -8,21 +8,21 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n",d->name);
+		else
+			printf("Name: (nil)\n");
+		if (d->age > 0)
+			printf("Age: %f\n",d->age);
+		else
+			printf("Age: (nil)\n");
 
-	if (d->name == NULL)
-		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
-
-	if (d->age < 0)
-		printf("Age: (nil)\n");
-	else
-		printf("Age: %f\n", d->age);
-
-	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
+		if (d->owner != NULL)
+			printf("Owner: %s\n",d->owner);
+		else
+			printf("Owner: (nil)\n");
+		            
+	}
 }
