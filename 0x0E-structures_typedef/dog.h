a61bc3e1d91e54b41968d1b4b2dadebb70124a56
Gnomedebian <yassineamgarou@gmail.com>
2023-07-17 19:35:20 +0000

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,16 @@
 #ifndef HEADER
 #define HEADER
-struct dog {
+/**
+ * struct dog - the object
+ * @name: his name
+ * @age: his age
+ * @owner: his owner
+ */
+struct dog
+{
 	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 
 #endif
