4b32866a7b359bdf8dc741056555465e8828dff4
Gnomedebian <yassineamgarou@gmail.com>
2023-07-19 16:56:00 +0000

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stddef.h>
 /**
  * int_index - searches for an integer
  * @array: array
@@ -12,16 +12,21 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (array != NULL && cmp != NULL)
 	{
+		if (size <= 0)
+		{
+			return (-1);
+		}
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
+		}
 		return (-1);
 	}
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]) != 0)
-		{
-			return (i);
-		}
-	}
-	return (-1);
+	else
+		return (0);
 }
