401eeb73ef1e7e82bd84c909ce474e2710be80c0
Gnomedebian <yassineamgarou@gmail.com>
2023-07-20 16:44:53 +0000

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers
+ * @separator: string
+ * @n: number of args
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list args;
+
+	va_start(args, n);
+	while (i < n)
+	{
+	int c = va_arg(args, int);
+
+	printf("%d", c);
+	if (i < n - 1)
+		printf("%s", separator);
+	i++;
+	}
+}
