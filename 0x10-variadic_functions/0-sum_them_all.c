49a990c491bed714b93bad2612f0cb5697ee3089
Gnomedebian <yassineamgarou@gmail.com>
2023-07-20 16:05:13 +0000

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,12 +12,16 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	va_list args;
 
+	if (n == 0)
+	{
+		return (0);
+	}
 	va_start(args, n);
-    
+
 	for (i = 0; i < n; i++)
 	{
 		sum = sum + va_arg(args, int);
 	}
-	return(sum);
+	return (sum);
 	va_end(args);
 }
