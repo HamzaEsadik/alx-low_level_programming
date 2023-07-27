e61812c1883a1aba4c2e804283760dee77993016
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-07-27 18:48:35 +0000

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_list - print linked list
+ * @h: pointer to linked list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+	const list_t *ptr = h;
+
+	if (ptr == NULL)
+	{
+		return (0);
+	}
+
+	while (ptr != NULL)
+	{
+		if (ptr->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", ptr->len, ptr->str);
+		}
+		ptr = ptr->next;
+		count++;
+	}
+	return (count);
+}
