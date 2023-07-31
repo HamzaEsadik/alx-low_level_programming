0b08f45570987ac92aeb495c87c4387444074fe6
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-07-31 23:55:11 +0100

    task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,10 +7,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *ptr;
 
-	while (ptr != NULL)
+	while (head)
 	{
-		ptr = head->next;
-		free(head);
-		head = ptr;
+		ptr = head;
+		head = head->next;
+		free(ptr);
 	}
 }
