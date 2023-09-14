62b4dddb8816b6233a919717634f0c89aeb2b18f
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-09-14 16:22:57 +0000

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,17 +8,24 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count;
-	const dlistint_t *temp;
-
-	temp = h;
-	count = 0;
-	while (temp->next != NULL)
+	if (h != NULL)
 	{
-		count++;
+		size_t count;
+		const dlistint_t *temp;
+
+		temp = h;
+		count = 0;
+		while (temp->next != NULL)
+		{
+			count++;
+			printf("%d\n", temp->n);
+			temp = temp->next;
+		}
 		printf("%d\n", temp->n);
-		temp = temp->next;
+		return (count + 1);
 	}
-	printf("%d\n", temp->n);
-	return (count + 1);
+	else
+	{
+		return (0);
+	}
 }
