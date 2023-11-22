58a437abb6d16d05686ed9c8fb29c6c3f631323b
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-11-22 20:10:06 +0100

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+''''''
 def island_perimeter(grid):
 	count = 0
 	for mylist in grid:
 		for i in mylist:
 			if i == 1:
 				count = count + 2
-	return count + 2
-
+	if count != 0:
+		return count + 2
+	else:
+		return 0
