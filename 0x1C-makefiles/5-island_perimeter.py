2005ecd0e2c1d76a3059717469bc2bcaea14dfcb
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-11-22 20:25:46 +0100

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
 '''island perimeter model'''
 def island_perimeter(grid):
-	'''
-	calcul perimeter of an island
+    '''
+    calcul perimeter of an island
 
-	args:
-		-grid: the list of arrays
+    args:
+        -grid: the list of arrays
 
-	return: the perimeter of the island
-	'''
-	count = 0
-	for mylist in grid:
-		for i in mylist:
-			if i == 1:
-				count = count + 2
-	if count != 0:
-		return count + 2
-	else:
-		return 0
+    return: the perimeter of the island
+    '''
+    count = 0
+    for mylist in grid:
+        for i in mylist:
+            if i == 1:
+            count = count + 2
+			else:
+				pass
+    if count != 0:
+        return count + 2
+    else:
+        return 0
+
+grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+print(island_perimeter(grid))
